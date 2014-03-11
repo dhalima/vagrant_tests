8cd566b7302e9619e312fd1e46c818d451c5e4d7
Daniel Henrique Alves Lima <dlima@ciant.com>
2014-03-11 15:27:48 -0300

    Examples

--- tests/_install_rh.sh
+++ tests/_install_rh.sh
@@ -3,3 +3,11 @@
 function install_apache() {
     yum install apache 2.2.3
 }
+
+function install_mysql() {
+    yum install mysql 2.2.3
+}
+
+function install_php() {
+    yum install php 5.2.14
+}
