c661fbc3d84720c14f12429f40e84523a893637c
Daniel Henrique Alves Lima <dlima@ciant.com>
2014-03-12 10:34:33 -0300

    Testing vg boxes

--- tests/localdev01/_rh_provision_functions.sh
+++ tests/localdev01/_rh_provision_functions.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-# rpm -Uvh http://mirror.webtatic.com/yum/el5/latest.rpm
-# rpm -Uvh http://dl.iuscommunity.org/pub/ius/stable/CentOS/5/x86_64/ius-release-1.0-11.ius.centos5.noarch.rpm
-# rpm -Uvh http://dl.iuscommunity.org/pub/ius/stable/CentOS/5/x86_64/epel-release-5-4.noarch.rpm
-
 function _y_install() {
     for _package_name_and_version; do true; done
-    _package_name=`echo ${_package_name_and_version} | cut -f1 -d'-'`
+
+    # http://www.linuxjournal.com/article/8919
+    _package_name=`echo ${_package_name_and_version%*-*-*}` # removes majorVersion-minorVersion from the right
+
+    echo "package name ${_package_name}"
 
     if ! yum list installed | grep -i "^${_package_name}"; then
-	yum -y install $*
+	yum --enablerepo=ius-archive -y install $*
 
 	return 0
     else
@@ -46,31 +46,36 @@ function install_apache() {
 
 function install_mysql() {
 #    yum install mysql-5.5.16
-    if _y_install --enablerepo=ius-archive mysql55-5.5.30-1.ius.centos5; then
+    if _y_install mysql55-5.5.30-1.ius.centos5; then
 	/sbin/chkconfig mysqld on
     fi
 }
 
 function install_phpadmin() {
 # http://vpsshell.co.uk/index.php/centosrhel-lamp-apache-php-and-mysql-in-linux/
-	_y_install --enablerepo=ius-archive phpmyadmin
+	_y_install phpmyadmin
     }
 
 function install_php() {
 # http://thepoch.com/2013/installing-php-5.2-on-centos-5-using-the-ius-community-project-repository.html
 #    yum install php-5.2.14
-    _y_install --enablerepo=ius-archive php52-5.2.17-6.ius.centos5
+    _y_install php52-5.2.17-6.ius.centos5
 }
 
 function install_apc() {
-    if ! pecl list | grep -i apc; then
+    if ! yum list installed | grep -i 'php-pear'; then
 	_php_version=`yum list installed | grep php | awk '{print $2}' | head -1`
 	_httpd_version=`yum list installed | grep httpd | awk '{print $2}' | head -1`
 	
-	_y_install --enablerepo=ius-archive php52-devel-${_php_version}
+	_y_install php52-devel-${_php_version}
 	_y_install httpd-devel-${_httpd_version}
-	_y_install php-pear pcre-devel gcc make
-	
+	_y_install php-pear
+	_y_install pcre-devel
+	_y_install gcc
+	_y_install make
+    fi
+
+    if ! pecl list | grep -i apc; then		
 	pecl install apc-3.1.9
 	
 	echo "extension=apc.so" > /etc/php.d/apc.ini
