8adca2e30b0927a27939df320ce377b3a7ababaf
Daniel Henrique Alves Lima <dlima@ciant.com>
2014-03-12 09:09:45 -0300

    Testing vg boxes

--- tests/localdev01/_rh_provision_functions.sh
+++ tests/localdev01/_rh_provision_functions.sh
@@ -4,21 +4,63 @@
 # rpm -Uvh http://dl.iuscommunity.org/pub/ius/stable/CentOS/5/x86_64/ius-release-1.0-11.ius.centos5.noarch.rpm
 # rpm -Uvh http://dl.iuscommunity.org/pub/ius/stable/CentOS/5/x86_64/epel-release-5-4.noarch.rpm
 
-function install_() {
-    rpm -Uvh http://dl.iuscommunity.org/pub/ius/stable/CentOS/5/x86_64/epel-release-5-4.noarch.rpm
-    rpm -Uvh http://dl.iuscommunity.org/pub/ius/stable/CentOS/5/x86_64/ius-release-1.0-11.ius.centos5.noarch.rpm
+function _y_install() {
+    _package_name=$1
+
+    if ! yum list installed | grep -i "^${_package_name}"; then
+	_package_version=
+
+	echo "x $2"
+	if [ -n "$2" ]; then
+	    _package_version="-$2"
+	fi
+	yum -y install ${_package_name}${_package_version}
+
+	return 0
+    else
+	return -1
+    fi
 }
+
+function _rpm() {
+    _package_name=`basename $1`
+    _package_name=`echo ${_package_name} | cut -f1 -d'.'`
+    
+    if ! rpm -qa | grep -i "^${_package_name}"; then
+	rpm -Uvh $1
+    fi
+}
+
+
+function prepare() {
+    #rpm -Uvh http://dl.iuscommunity.org/pub/ius/stable/CentOS/5/x86_64/epel-release-5-4.noarch.rpm
+    #rpm -Uvh http://dl.iuscommunity.org/pub/ius/stable/CentOS/5/x86_64/ius-release-1.0-11.ius.centos5.noarch.rpm
+    
+    _rpm http://dl.iuscommunity.org/pub/ius/stable/CentOS/5/x86_64/epel-release-5-4.noarch.rpm
+    _rpm http://dl.iuscommunity.org/pub/ius/stable/CentOS/5/x86_64/ius-release-1.0-11.ius.centos5.noarch.rpm
+
+    #yum install yum-fastestmirror
+    _y_install yum-fastestmirror
+}
+
 
 function install_apache() {
 #    yum install apache-2.2.3
-    yum -y install httpd-2.2.3-82.el5.centos
-    /sbin/chkconfig httpd on
+    #yum -y install httpd-2.2.3-82.el5.centos
+    if _y_install httpd 2.2.3-82.el5.centos; then
+	/sbin/chkconfig httpd on
+    fi
 }
 
 function install_mysql() {
 #    yum install mysql-5.5.16
     yum --enablerepo=ius-archive -y install mysql55-5.5.30-1.ius.centos5
+    /sbin/chkconfig mysqld on
 }
+
+function install_phpadmin() {
+	yum -y install phpmyadmin
+    }
 
 function install_php() {
 # http://thepoch.com/2013/installing-php-5.2-on-centos-5-using-the-ius-community-project-repository.html
--- tests/localdev01/provision.sh
+++ tests/localdev01/provision.sh
@@ -4,7 +4,9 @@ echo $0
 
 . /vagrant/_rh_provision_functions.sh
 
-install_mysql
+prepare
+#install_mysql
 install_apache
-install_php
-install_apc
+#install_php
+#install_phpadmin
+#install_apc
