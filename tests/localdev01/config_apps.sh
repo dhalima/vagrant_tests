7954e182da79d4c8d01d91ad8cc33a685115e6bc
Daniel Henrique Alves Lima <dlima@ciant.com>
2014-03-14 14:12:02 -0300

    Installing apps

--- tests/localdev01/config_apps.sh
+++ tests/localdev01/config_apps.sh
@@ -5,7 +5,7 @@ function _configure_apache() {
     _conf_name=`basename ${_conf_path}`
 
     if [ ! -e /etc/httpd/conf.d/${_conf_name} ]; then
-	echo ln -n -s ${_conf_path} /etc/httpd/conf.d/${_conf_name}
+	echo "ln -n -s ${_conf_path} /etc/httpd/conf.d/${_conf_name}"
     fi
 }
 
@@ -14,16 +14,19 @@ function _configure_mysql() {
     _user=`basename $1`
 
     # http://www.linuxjournal.com/article/8919
-    _user=`echo ${_user%*.sql}` # removes .sql from the right
+    _user=`echo ${_user%*.mysql}` # removes .sql from the right
 
-    if ! mysql -u root -e "SELECT SCHEMA_NAME FROM INFORMATION_SCHEMA.SCHEMATA WHERE SCHEMA_NAME = '${_user}'" | grep -i ${_user}
+
+    #echo "xxx ${_user}"
+
+    if ! mysql -u root -e "SELECT SCHEMA_NAME FROM INFORMATION_SCHEMA.SCHEMATA WHERE SCHEMA_NAME = '${_user}'" | grep -i -q ${_user}; then
 #	mysql -u root << END
 #CREATE USER '${_user}'@'localhost' IDENTIFIED BY '${_user}';
 #CREATE DATABASE ${_user}
 #GRANT ALL PRIVILEGES ON ${_user}.* TO '${_user}'@'localhost';
 #END
     
-	echo cat ${_dump_path} | mysql -u ${_user} -p${_user} ${_user}
+	echo "cat ${_dump_path} | mysql -u ${_user} -p${_user} ${_user}"
     fi
 }
 
