426aa525ffa197c859f7ace07359d7fe8296c455
Daniel Henrique Alves Lima <dlima@ciant.com>
2014-03-14 14:06:58 -0300

    Installing apps

--- tests/localdev01/config_apps.sh
+++ tests/localdev01/config_apps.sh
@@ -4,30 +4,34 @@ function _configure_apache() {
     _conf_path=$1
     _conf_name=`basename ${_conf_path}`
 
-    echo ln -n -s ${_conf_path} /etc/httpd/conf.d/${_conf_name}
+    if [ ! -e /etc/httpd/conf.d/${_conf_name} ]; then
+	echo ln -n -s ${_conf_path} /etc/httpd/conf.d/${_conf_name}
+    fi
 }
 
-function configure_mysql() {
+function _configure_mysql() {
     _dump_path=$1    
     _user=`basename $1`
 
     # http://www.linuxjournal.com/article/8919
     _user=`echo ${_user%*.sql}` # removes .sql from the right
 
-mysql -u root << END
-CREATE USER '${_user}'@'localhost' IDENTIFIED BY '${_user}';
-CREATE DATABASE ${_user}
-GRANT ALL PRIVILEGES ON ${_user}.* TO '${_user}'@'localhost';
-END
-
-cat ${_dump_path} | mysql -u ${_user} -p${_user} ${_user}
+    if ! mysql -u root -e "SELECT SCHEMA_NAME FROM INFORMATION_SCHEMA.SCHEMATA WHERE SCHEMA_NAME = '${_user}'" | grep -i ${_user}
+#	mysql -u root << END
+#CREATE USER '${_user}'@'localhost' IDENTIFIED BY '${_user}';
+#CREATE DATABASE ${_user}
+#GRANT ALL PRIVILEGES ON ${_user}.* TO '${_user}'@'localhost';
+#END
+    
+	echo cat ${_dump_path} | mysql -u ${_user} -p${_user} ${_user}
+    fi
 }
 
-export -f _configure_apache
+export -f _configure_apache _configure_mysql
 
-#find /vagrant/sites/conf -name '*.apache' -type f -print0 | xargs -I {} -0 bash -c '_x "$@"' _ {}
-find sites/conf -name '*.apache' -type f -print0 | xargs -I {} -0 bash -c '_configure_apache "$@"' _ {}
-
+#find sites/conf -name '*.apache' -type f -print0 | xargs -I {} -0 bash -c '_configure_apache "$@"' _ {}
+find /vagrant/apps/conf -name '*.apache' -type f -print0 | xargs -I {} -0 bash -c '_configure_apache "$@"' _ {}
+find /vagrant/apps/dump -name '*.mysql' -type f -print0 | xargs -I {} -0 bash -c '_configure_mysql "$@"' _ {}
 
 
 #CREATE USER 'drupal'@'localhost' IDENTIFIED BY 'drupal';
